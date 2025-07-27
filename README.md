2eb11be813c1084f23f2b1b6f67ca402a235ffe6
Dimitrs Oiko <dimoiko100@gmail.com>
2025-07-27 21:04:53 +0300

    docs

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 ### jvob
 
 Use jvob if you have a json bytes vector and you want to find the indexs (offsets) of the json values. This can be useful in cases where given a json bytes stream we want to know the values bytes offsets in order to apply a wordlist or other modifications to only data of interest. 
+
+Run the test `main.rs` to check a json file:
+```bash
+cargo run -- test.jso
+```
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 use std::{fs, io::Read};
-
+use std::env;
+use std::process;
 use jvob::json_values_byte_offsets;
 use jvob::TrimString;
 
 fn main() {
-    let mut file = fs::File::open("./test.json").unwrap();
+    let args: Vec<String> = env::args().collect();
+    if args.len() <= 1 {
+        println!("No file was provided: cargo run -- test.json");
+        process::exit(1);
+    }
+    let file_name = &args[1];
+    let mut file = fs::File::open(file_name).unwrap();
     let mut json_bytes: Vec<u8> = Vec::new();
     let _ = file.read_to_end(&mut json_bytes);
 
@@ -12,9 +19,9 @@ fn main() {
         Ok(jo) => {
             for j in jo {
                 if *j.r#type() == jvob::JType::JString {
-                    println!("map: {:?}", j.value().trim_edges_bytes().unwrap());
+                    println!("string: {:?}", j.value().trim_edges_bytes().unwrap());
                 } else {
-                    println!("map: {:?}", j.value());
+                    println!("map: {}", j.value());
                 }
             }
         },
